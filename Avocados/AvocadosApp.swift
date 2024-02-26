433071cc30a25870fbc100634cadcdf1456f3b14
Kota Suzuki <addressuzuki@gmail.com>
2024-02-27 08:52:13 +0900

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by 鈴木航太 on 2024/02/27.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
